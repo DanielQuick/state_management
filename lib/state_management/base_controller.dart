f58f3681ef2da50b1c0501fd60ab7cf26013ec2c
DanielQuick <danielquick14@gmail.com>
2022-01-06 12:50:58 -0600

    Add disposed flag to controller

--- lib/state_management/base_controller.dart
+++ lib/state_management/base_controller.dart
@@ -3,5 +3,13 @@ import 'package:flutter/material.dart';
 class BaseController extends ChangeNotifier {
   final BuildContext context;
 
+  bool disposed = false;
+
   BaseController(this.context);
+
+  @override
+  dispose() {
+    disposed = true;
+    super.dispose();
+  }
 }
